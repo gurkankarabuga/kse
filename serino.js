e04b34589b6f873874429be8b1531055f368b285
gurkankarabuga <a.gurkankarabuga@gmail.com>
2025-03-15 04:28:42 +0300

    Update serino.js

--- serino.js
+++ serino.js
@@ -3,9 +3,6 @@ function verifySerialNumber() {
     const resultContainer = document.getElementById('result-container');
 
     if (serialNumberInput === '2025kV8QjJFL') {
-        document.getElementById('company-name').innerText = 'KSE İçecekleri A.Ş';
-        document.getElementById('product-quantity').innerText = '1.500.000';
-        document.getElementById('product-name').innerText = 'Kutu İçecek';
         document.getElementById('carbon-result').innerText = '11.70 kg CO2e';
         document.getElementById('serial-number').innerText = '2025kV8QjJFL';
         document.getElementById('emission-label').innerText = 'B';
